2d78bac4bc6a449b23947328753459b7ffa9882c
ennamarie19 <dallmeyerm@att.net>
2022-10-17 16:30:00 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_html.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+import atheris
+import sys
+import io
+import tempfile
+
+with atheris.instrument_imports():
+    import mistune
+
+
+def TestOneInput(data):
+    fdp = atheris.FuzzedDataProvider(data)
+    file_data = fdp.ConsumeBytes(atheris.ALL_REMAINING)
+    temp_file = tempfile.NamedTemporaryFile()
+    temp_file.write(file_data)
+    temp_file.flush()
+    fd = temp_file.name
+
+
+    try:
+        mistune.html(fd)
+
+    except (NotImplementedError, AttributeError, DeprecationWarning):
+        pass
+
+    temp_file.close()
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
